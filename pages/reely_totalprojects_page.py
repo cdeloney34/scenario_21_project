fb49b91ddf260157739628271f354536d8e04128
Cedric D <cedricdeloney@gmail.com>
2024-05-30 21:32:12 -0500

    Adding project 1 assignment files

--- pages/reely_totalprojects_page.py
+++ pages/reely_totalprojects_page.py
@@ -5,14 +5,14 @@ from time import sleep
 
 class ReelyTotalprojectsPage(Page):
     reely_verify_text = (By.XPATH,"//*[text()='Total projects']")
-    filter_button = (By.XPATH,"//a[@wized='openFiltersWindow']")
+    filter_button = (By.CSS_SELECTOR,"a[class='filter-button w-inline-block']")
     out_of_stock_button = (By.XPATH, "//div[@wized='priorityStatusOutOfStock']")
     apply_filter_button = (By.XPATH, "//a[@wized='applyFilterButton']")
     verification_status = (By.XPATH,"//*[text()='Out of stock']")
 
 
     def verify_right_page_open(self):
-        actual_text = self.find_element(*self.verify_text).text
+        actual_text = self.find_element(*self.reely_verify_text).text
         expected_text = "Total projects"
         assert expected_text in actual_text, f'Error! Text {expected_text} is not in {actual_text}'
         sleep(10)
@@ -22,7 +22,8 @@ class ReelyTotalprojectsPage(Page):
         self.click(*self.filter_button)
         self.click(*self.out_of_stock_button)
         self.click(*self.apply_filter_button)
+        sleep(15)
 
 
-
-
+    def verify_out_of_stock_tag(self):
+        self.find_elements(*self.verification_status)
