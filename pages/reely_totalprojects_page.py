8d6b21d95df9b5a0261270d5d10ac4bb89e6be07
Cedric D <cedricdeloney@gmail.com>
2024-05-31 19:14:09 -0500

    updated Internship project files

--- pages/reely_totalprojects_page.py
+++ pages/reely_totalprojects_page.py
@@ -4,12 +4,11 @@ from time import sleep
 
 
 class ReelyTotalprojectsPage(Page):
-    reely_verify_text = (By.XPATH,"//*[text()='Total projects']")
-    filter_button = (By.CSS_SELECTOR,"a[class='filter-button w-inline-block']")
+    reely_verify_text = (By.XPATH, "//*[text()='Total projects']")
+    filter_button = (By.CSS_SELECTOR, "a[class='filter-button w-inline-block']")
     out_of_stock_button = (By.XPATH, "//div[@wized='priorityStatusOutOfStock']")
     apply_filter_button = (By.XPATH, "//a[@wized='applyFilterButton']")
-    verification_status = (By.XPATH,"//*[text()='Out of stock']")
-
+    verification_status = (By.XPATH, "//*[text()='Out of stock']")
 
     def verify_right_page_open(self):
         actual_text = self.find_element(*self.reely_verify_text).text
@@ -17,13 +16,16 @@ class ReelyTotalprojectsPage(Page):
         assert expected_text in actual_text, f'Error! Text {expected_text} is not in {actual_text}'
         sleep(10)
 
-
     def filter_by_out_of_stock_status(self):
         self.click(*self.filter_button)
         self.click(*self.out_of_stock_button)
         self.click(*self.apply_filter_button)
         sleep(15)
 
-
     def verify_out_of_stock_tag(self):
-        self.find_elements(*self.verification_status)
+        expected_tag = "Out of stock"
+        out_of_stock_elements = self.find_elements(*self.verification_status)
+        for element in out_of_stock_elements:
+            actual_tag = element.text
+            if expected_tag == actual_tag:
+                    print(expected_tag)
